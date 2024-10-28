14e60eafee0cca310697c826a9814892376a123f
lucas Liu <lantasy.io@gmail.com>
2024-10-28 16:41:26 -0700

    update base url

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/*
+ * @Author: lucas Liu lantasy.io@gmail.com
+ * @Date: 2024-10-22 13:24:42
+ * @LastEditTime: 2024-10-28 16:41:15
+ * @Description:
+ */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/lucasblog/',
+});
